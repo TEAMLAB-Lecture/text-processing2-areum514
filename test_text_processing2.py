f722d05823b64a57629f010c9f39e0f5e67752e9
areum514 <21500514@handong.edu>
2021-01-19 17:40:53 +0900

    Update my code

--- test_text_processing2.py
+++ test_text_processing2.py
@@ -84,5 +84,3 @@ class TestTextProcessing(unittest.TestCase):
         pred = tp.to_camel_case(test_str)
         self.assertEqual(pred, ".....")
 
-if __name__ == "__main__":
-        unittest.main()
